dfdbb78621f5929ce8e120f8cb9e3ca98d895ba3
davidrc2009 <67182999+davidrc2009@users.noreply.github.com>
2022-09-22 17:09:39 -0500

    Update llama.ps1

--- llama.ps1
+++ llama.ps1
@@ -1,18 +1,17 @@
 ﻿ function llamar { 
-#remove old task
-Unregister-ScheduledTask -TaskName "llama2" -Confirm:$false
-# Create task action
-$taskAction = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/davidrc2009/test/main/RemoveLocalAdminV1.ps1'))" # Specify what program to run and with its parameters
-
-# Create a trigger (Mondays at 4 AM)
-$taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(7)
-# The user to run the task
+# Remover tarea existente
+Unregister-ScheduledTask -TaskName "llama" -Confirm:$false
+# Crear una acción para la tarea que descargará RemoveLocalAdminV1.ps1
+$taskAction = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/davidrc2009/test/main/RemoveLocalAdminV1.ps1'))" 
+# Crear un trigger para que la tarea se ejecute inmediatamente
+$taskTrigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds(1)
+# Especificar el usuario con el que se ejecutará la tarea
 $taskUser = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
-# The name of the scheduled task.
-$taskName = "llama2"
-# Describe the scheduled task.
-$description = "Test"
-# Register the scheduled task
+# Especificar un nombre para la tarea
+$taskName = "llama"
+# Especificar una descripción para la tarea
+$description = "Esta tarea llamará al script RemoveLocaladminV1.ps1 para que el usuario que enrolo el dispositivo a Azure AD no sea administrador"
+# Registrar la tarea
 Register-ScheduledTask -TaskName $taskName -Action $taskAction -Trigger $taskTrigger -Principal $taskUser -Description $description
 }
 llamar
